c7bea9ecf91c7b7c27b0dc74884fbab6dc0388c6
mayorr-star <immaboat11@gmail.com>
2023-11-20 17:40:30 +0200

    Returned the value of playround function instead of console logging

---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+Declare a function called getComputerChoice that will randomly select rock, paper or scissors as the computer's choice
+*/
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    return computerChoice;
+}
+
+function getPlayerChoice() {
+    const playerChoice = prompt("Please enter your choice").toLowerCase()
+    return playerChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! paper beats rock"
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! paper beats rock"
+    } else if (playerSelection ==="rock" && computerSelection === "scissors") {
+        return "You win! rock beats scissors"
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! rock beats scissors"
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! paper beats scissors"
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! scissors beats paper"
+    } else {
+        return "It's a draw"
+    }
+}
+
+console.log(playRound(getPlayerChoice(), getComputerChoice()))
