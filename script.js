3f6e2e94f199b96ba08d73a993cd5650f2dac176
mayorr-star <immaboat11@gmail.com>
2023-11-22 18:54:12 +0200

    Added a function to play a round of five games and displays the winner and the score at the end of each round

--- script.js
+++ script.js
@@ -1,40 +1,58 @@
 /*
 Declare a function called getComputerChoice that will randomly select rock, paper or scissors as the computer's choice
 */
-
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
 
+
 function getPlayerChoice() {
     const playerChoice = prompt("Please enter your choice").toLowerCase();
     return playerChoice;
 }
 
+let playerScore = 0;
+let computerScore = 0; 
+let result = null;
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You lose! paper beats rock"
+        result =  "You lose! paper beats rock"
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win! paper beats rock"
+        result =  "You win! paper beats rock"
     } else if (playerSelection ==="rock" && computerSelection === "scissors") {
-        return "You win! rock beats scissors"
+        result =  "You win! rock beats scissors"
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You lose! rock beats scissors"
+        result =  "You lose! rock beats scissors"
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You lose! paper beats scissors"
+        result =  "You lose! paper beats scissors"
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win! scissors beats paper"
+        result =  "You win! scissors beats paper"
     } else {
-        return "It's a draw"
+        result =  "It's a draw"
     }
+    return result
+    
 }
-console.log(playRound(getPlayerChoice(), getComputerChoice()))
-/* 
-function game(rounds) {
-    for (let i = 0; i < rounds; i++) {
-        playRound(getPlayerChoice(), getComputerChoice())
+
+function game() {
+    let index = 0;
+    while (index < 5) {
+        console.log(playRound(getPlayerChoice(), getComputerChoice()))
+        index++;
+        switch (true) {
+            case result.indexOf("You win") !== -1:
+                playerScore++;
+                console.log(`playerScore: ${playerScore}`);
+                break;
+            case result.indexOf("You lose") !== -1:
+                computerScore++;
+                console.log(`computerScore: ${computerScore}`)
+                break;
+        }
     }
+    console.log(playerScore > computerScore ? "You win!" : "You lose!");
 }
-console.log(game(5)) */
+game()
